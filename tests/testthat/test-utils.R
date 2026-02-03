acbb80913288af35202bbaf4cef4d03fc20dbbbb
Polymerase3 <polimerazaiii@gmail.com>
2026-02-03 17:45:17 +0100

    Changed function names in the test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,11 @@
 skip_if_not_installed("chk")
 
 # -------------------------------------------------------------------------
-# .chk_cond ----------------------------------------------------------------
+# .ph_check_cond ------------------------------------------------------------
 # -------------------------------------------------------------------------
-test_that(".chk_cond emits errors and warnings correctly", {
+test_that(".ph_check_cond emits errors and warnings correctly", {
   expect_error(
-    .chk_cond(TRUE,
+    .ph_check_cond(TRUE,
       "boom",
       error = TRUE
     ),
@@ -13,7 +13,7 @@ test_that(".chk_cond emits errors and warnings correctly", {
   )
 
   expect_warning(
-    .chk_cond(TRUE,
+    .ph_check_cond(TRUE,
       "soft boom",
       error = FALSE
     ),
@@ -21,37 +21,37 @@ test_that(".chk_cond emits errors and warnings correctly", {
   )
 
   # When condition is FALSE nothing happens
-  expect_silent(.chk_cond(FALSE, "no-op"))
+  expect_silent(.ph_check_cond(FALSE, "no-op"))
 })
 
 # -------------------------------------------------------------------------
-# .chk_extension -----------------------------------------------------------
+# .ph_check_extension ------------------------------------------------------
 # -------------------------------------------------------------------------
-test_that(".chk_extension passes correct ext and fails otherwise", {
+test_that(".ph_check_extension passes correct ext and fails otherwise", {
   # good
   expect_silent(
-    .chk_extension("file.TSV", "file", c("tsv", "csv"))
+    .ph_check_extension("file.TSV", "file", c("tsv", "csv"))
   )
 
   # bad
   expect_error(
-    .chk_extension("file.bad", "file", c("tsv", "csv")),
+    .ph_check_extension("file.bad", "file", c("tsv", "csv")),
     class = "chk_error"
   )
 })
 
 # -------------------------------------------------------------------------
-# .chk_null_default --------------------------------------------------------
+# .ph_check_null_default ---------------------------------------------------
 # -------------------------------------------------------------------------
-test_that(".chk_null_default returns original or default", {
+test_that(".ph_check_null_default returns original or default", {
   expect_equal(
-    .chk_null_default(5, "x", "m", 10),
+    .ph_check_null_default(5, "x", "m", 10),
     5
   )
 
   withr::with_options(list(warn = 1), {
     expect_warning(
-      out <- .chk_null_default(NULL, "x", "m", 10),
+      out <- .ph_check_null_default(NULL, "x", "m", 10),
       label = "method"
     )
     expect_equal(out, 10)
@@ -59,56 +59,56 @@ test_that(".chk_null_default returns original or default", {
 })
 
 # -------------------------------------------------------------------------
-# .chk_path ----------------------------------------------------------------
+# .ph_check_path -----------------------------------------------------------
 # -------------------------------------------------------------------------
-test_that(".chk_path validates path + ext", {
+test_that(".ph_check_path validates path + ext", {
   tmp <- withr::local_tempfile(fileext = ".csv")
   writeLines("test", tmp)
 
   # passes
   expect_silent(
-    .chk_path(tmp, "arg", extension = "csv")
+    .ph_check_path(tmp, "arg", extension = "csv")
   )
 
   # fails on extension
   expect_error(
-    .chk_path(tmp, "arg", extension = "tsv"),
+    .ph_check_path(tmp, "arg", extension = "tsv"),
     class = "chk_error"
   )
 
   # fails on non-existent path
   expect_error(
-    .chk_path(file.path(tempdir(), "nope.csv"), "arg", extension = "csv"),
+    .ph_check_path(file.path(tempdir(), "nope.csv"), "arg", extension = "csv"),
     class = "chk_error"
   )
 })
 
-test_that(".chk_path works for folders", {
+test_that(".ph_check_path works for folders", {
   tmp <- withr::local_tempdir()
 
   # Default is is_dir=FALSE
   expect_error(
-    .chk_path(tmp, "arg")
+    .ph_check_path(tmp, "arg")
   )
 
   # Explicit *file* check
   expect_error(
-    .chk_path(tmp, "arg", is_dir=FALSE)
+    .ph_check_path(tmp, "arg", is_dir=FALSE)
   )
 
   # Correct folder check
   expect_silent(
-    .chk_path(tmp, "arg", is_dir=TRUE)
+    .ph_check_path(tmp, "arg", is_dir=TRUE)
   )
 
   # Both folder and extension should fail
   expect_error(
-    .chk_path(tmp, "arg", c("txt", "pqt"), is_dir=TRUE)
+    .ph_check_path(tmp, "arg", c("txt", "pqt"), is_dir=TRUE)
   )
 
   # Non existing directory should fail
   expect_error(
-    .chk_path("this/path/should/definetly/not/exist", "arg", is_dir=TRUE)
+    .ph_check_path("this/path/should/definetly/not/exist", "arg", is_dir=TRUE)
   )
 })
 
